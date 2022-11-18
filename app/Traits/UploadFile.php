f13019668cb48e177d8f538f882e1867c9c1598a
ashrafAli23 <ashrafammar230@gmail.com>
2022-11-18 15:14:06 +0200

    create function to upload multiple file

--- app/Traits/UploadFile.php
+++ app/Traits/UploadFile.php
@@ -10,12 +10,24 @@ use Illuminate\Support\Str;
 
 trait UploadFile
 {
-    public function uploadFile(Request $request, string $path): string
+    public function uploadFile(Request $request, string $file_name = null, string $path): string
     {
-        $imageName = Str::replace(' ', '', $request->file("image")->getClientOriginalName());
+        $imageName = Str::replace(' ', '', $request->file($file_name ?? "image")->getClientOriginalName());
         $path = $request->file("image")->storeAs($path, rand(1, 99999) . $imageName, 'public');
 
         return 'storage/' . $path;
+        // return asset('storage/app/public/' . $path);
+    }
+
+    public function uploadMultipleFile(Request $request, string $file_name = null, string $path): array
+    {
+        $image_data = [];
+        foreach ($request->file($file_name ?? 'images') as $image) {
+            $imageName = Str::replace(' ', '', $image->getClientOriginalName());
+            $image_data[] = 'storage/' . $image->storeAs($path, rand(1, 99999) . $imageName, 'public');
+        }
+
+        return $image_data;
         // return asset('storage/app/public/' . $path);
     }
 
