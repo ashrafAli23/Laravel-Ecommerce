980accf48497744e2efbc1755d8ab80a90d5bc3d
ashrafAli23 <ashrafammar230@gmail.com>
2022-11-24 10:42:00 +0200

    fix bug

--- app/Traits/UploadFile.php
+++ app/Traits/UploadFile.php
@@ -13,7 +13,7 @@ trait UploadFile
     public function uploadFile(Request $request, string $file_name = null, string $path): string
     {
         $imageName = Str::replace(' ', '', $request->file($file_name ?? "image")->getClientOriginalName());
-        $path = $request->file("image")->storeAs($path, rand(1, 99999) . $imageName, 'public');
+        $path = $request->file($file_name ?? "image")->storeAs($path, rand(1, 99999) . $imageName, 'public');
 
         return 'storage/' . $path;
         // return asset('storage/app/public/' . $path);
