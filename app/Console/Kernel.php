06cdb958e6c61b9dcb72af54c2a2bd0f8ec84edd
ashrafAli23 <ashrafammar230@gmail.com>
2022-12-21 17:06:49 +0200

    update laravel and php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,10 +1,7 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Console;
 
-use App\Jobs\EmptyExpiredCarts;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,9 +13,9 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule): void
+    protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new EmptyExpiredCarts())->monthly();
+        // $schedule->command('inspire')->hourly();
     }
 
     /**
@@ -26,9 +23,9 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands(): void
+    protected function commands()
     {
-        $this->load(__DIR__ . '/Commands');
+        $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
     }
