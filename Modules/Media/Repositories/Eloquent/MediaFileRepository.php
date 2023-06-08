3ca96fb2313ff1f2bd1b57e8fe1d0f5a4e39c829
ashrafAli23 <ashrafammar230@gmail.com>
2023-06-08 12:35:10 +0300

    fix bugs

--- Modules/Media/Repositories/Eloquent/MediaFileRepository.php
+++ Modules/Media/Repositories/Eloquent/MediaFileRepository.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Modules\Media\Repositories\Eloquent;
 
 use Modules\Common\Repositories\BaseRepository;
-use Modules\Media\Repositories\Interfaces\IMediaFile;
+use Modules\Media\Repositories\Interfaces\IMediaFileRepository;
 
-class MediaFileRepository extends BaseRepository implements IMediaFile
+class MediaFileRepository extends BaseRepository implements IMediaFileRepository
 {
 }
