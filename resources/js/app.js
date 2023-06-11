de9b32fdd45700a09042d7f79ea3933aff772d6b
ashrafAli23 <ashrafammar230@gmail.com>
2023-06-11 14:34:28 +0300

    add pusher

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
+
+// listen on private channel
+// let channel = Echo.private("App.Models.User.{id}");
+
+// // listen on notification class
+// channel.notification(function (data) {
+//     alert(JSON.stringify(data));
+// });
